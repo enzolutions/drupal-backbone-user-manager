a3cf0af1dbc251e190503c67b41c589598699f26
enzo - Eduardo Garcia <enzo@anexusit.com>
2013-10-15 14:36:49 -0600

    Added CRUD operations

--- js/backbone_user_manager_app.js
+++ js/backbone_user_manager_app.js
@@ -1,6 +1,21 @@
 (function($) {
-  Drupal.behaviors.backbone_user_manager = {
+  $.fn.serializeObject = function() {
+      var o = {};
+      var a = this.serializeArray();
+      $.each(a, function() {
+          if (o[this.name] !== undefined) {
+              if (!o[this.name].push) {
+                  o[this.name] = [o[this.name]];
+              }
+              o[this.name].push(this.value || '');
+          } else {
+              o[this.name] = this.value || '';
+          }
+      });
+      return o;
+    };
 
+  Drupal.behaviors.backbone_user_manager = {
     attach: function() {
       // Create a Collection from a our backbone_user_manager view.
       var UserCollection = new Drupal.Backbone.Collections.UserIndex({});
@@ -10,6 +25,28 @@
         el: '#backbone-user-manager-app',
         initialize: function() {
           Drupal.Backbone.View.prototype.initialize.apply(this);
+
+          //Set Token to avoid Error: 'CSRF validation failed'.
+          var session_token = '';
+          $.ajax({
+            url:"/services/session/token",
+            type:"get",
+            dataType:"text",
+            error:function (jqXHR, textStatus, errorThrown) {
+              alert(errorThrown);
+            },
+            success: function (token) {
+                $.ajaxSetup({
+                  beforeSend: function (request) {
+                    request.setRequestHeader("X-CSRF-Token", token);
+                  },
+                });
+            }
+          });
+
+          //Set messages area
+          $('#header').after('<div id="messages"></div>');
+
         },
          // Render the list of users.
          render: function() {
@@ -28,8 +65,75 @@
       //
       // Then all we need to do is create an instance of our app view!
       var app = new AppView();
-      app.render();
 
+      var UserEditView = Backbone.View.extend({
+        el: '#backbone-user-manager-app',
+        render: function(options) {
+          var that = this;
+          if(options.uid) {
+            that.user = new Drupal.Backbone.Models.User({uid: options.uid});
+            that.user.fetch({
+              success: function (user) {
+                var template = _.template($('#backbone-user-manager-user-edit-template').html(), {user: user});
+                that.$el.html(template);
+              },
+              error: function (user, response) {
+                $("#messages").html('<div class="section clearfix"><div class="messages error">' + Drupal.t('Invalid User ID') + options.uid + '</div></div>');
+                that.$el.html('<div><a href="#">'  + Drupal.t('Back to Home') + '</a></div>');
+              },
+            });
+          } else {
+            var template = _.template($('#backbone-user-manager-user-edit-template').html(), {user: null});
+            that.$el.html(template);
+          }
+        },
+        events: {
+          'submit .edit-user-form': 'saveUser',
+          'click .delete': 'deleteUser'
+        },
+        saveUser: function (ev) {
+          var userDetails = $(ev.currentTarget).serializeObject();
+          var user = new Drupal.Backbone.Models.User();
+          user.save(userDetails, {
+            success: function (user) {
+              router.navigate('', {trigger:true});
+            },
+            error: function (user, response) {
+              $("#messages").html('<div class="section clearfix"><div class="messages error">' + response.statusText + '</div></div>');
+            },
+          });
+          return false;
+        },
+        deleteUser: function (ev) {
+          this.user.destroy({
+            success: function () {
+              router.navigate('', {trigger:true});
+            }
+          });
+          return false;
+        },
+      });
+
+      var Router = Backbone.Router.extend({
+        routes: {
+          "": "home",
+          "edit/:uid": "edit",
+          "new": "edit",
+        }
+      });
+
+      var router = new Router();
+      router.on('route:home', function() {
+        // render user list
+        app.render();
+      });
+
+      var userEditView = new UserEditView();
+      router.on('route:edit', function(uid) {
+        userEditView.render({uid: uid});
+      });
+
+      Backbone.history.start();
     },
   };
 })(jQuery);
